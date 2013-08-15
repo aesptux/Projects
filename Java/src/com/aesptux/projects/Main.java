328b6951381a6ce6aa88b76b90fb90a8b0e7e492
Adrian Espinosa <aespinosamoreno@gmail.com>
2013-08-15 20:55:12 +0200

    Updating README and main
    Signed-off-by: Adrian Espinosa <aespinosamoreno@gmail.com>

--- Java/src/com/aesptux/projects/Main.java
+++ Java/src/com/aesptux/projects/Main.java
@@ -4,32 +4,63 @@ import com.aesptux.projects.Numbers.Factorial;
 import com.aesptux.projects.Numbers.Fibonacci;
 import com.aesptux.projects.Classes.Inventory.Product;
 import com.aesptux.projects.Classes.Inventory.Inventory;
+import com.aesptux.projects.Text.CountVowels;
+import com.aesptux.projects.Text.PigLatin;
+import com.aesptux.projects.Text.ReverseString;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        FACTORIAL
+         */
         //System.out.println(Factorial.factorial_loop());
         //System.out.println(Factorial.factorial_recursive(10));
 
+        /*
+        FIBONACCI
+         */
         // System.out.println(Fibonacci.Fibonacci(4));
 
-        // Inventory
-        Product mouse = new Product();
-        mouse.id = 1;
-        mouse.price = 50;
-        mouse.quantity = 2;
-        Product keyboard = new Product();
-        keyboard.id = 2;
-        keyboard.price = 80;
-        keyboard.quantity = 4;
+        /*
+        INVENTORY
+         */
+//        Product mouse = new Product();
+//        mouse.id = 1;
+//        mouse.price = 50;
+//        mouse.quantity = 2;
+//        Product keyboard = new Product();
+//        keyboard.id = 2;
+//        keyboard.price = 80;
+//        keyboard.quantity = 4;
+//
+//        Inventory inventory = new Inventory();
+//        inventory.addProduct(mouse);
+//        inventory.addProduct(keyboard);
+//
+//        System.out.println("There are " + inventory.getTotalProducts() +
+//                " items in the inventory");
+//        System.out.println("The total value of the inventory is " +
+//                inventory.getTotalValue());
 
-        Inventory inventory = new Inventory();
-        inventory.addProduct(mouse);
-        inventory.addProduct(keyboard);
+        /*
+        REVERSE STRING
+         */
+        System.out.println(ReverseString.ReverseString("Reverse this string"));
 
-        System.out.println("There are " + inventory.getTotalProducts() +
-                " items in the inventory");
-        System.out.println("The total value of the inventory is " +
-                inventory.getTotalValue());
+        /*
+        PIG LATIN
+         */
+        System.out.println(PigLatin.PigLatin("Banana"));
+
+        /*
+        COUNT VOWELS
+         */
+        String toCountVowels = "Magical Software";
+        Object[] result = CountVowels.CountVowels(toCountVowels);
+        System.out.println("Total vowels in " + toCountVowels + " = " +
+                result[1]);
+        System.out.println(result[0]);
+
     }
 }
