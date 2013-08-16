263b85e9316dd8ebc11d45f84687ec3cb4ca2521
Adrian Espinosa <aespinosamoreno@gmail.com>
2013-08-16 19:16:28 +0200

    update readme and main
    Signed-off-by: Adrian Espinosa <aespinosamoreno@gmail.com>

--- C++/projects_cpp/main.cpp
+++ C++/projects_cpp/main.cpp
@@ -8,12 +8,24 @@
 
 #include <iostream>
 #include "Numbers/factorial.h"
+#include "Text/ReverseString.h"
+#include "Text/PigLatin.h"
+#include "Text/CountVowels.h"
 
 
 int main(int argc, const char * argv[])
 {
-
-    std::cout << Factorial::factorial_loop(20);
+    /* FACTORIAL */
+    std::cout << Factorial::factorial_loop(20) << std::endl;
+    
+    /* REVERSE STRING */
+    std::cout << ReverseString::strrev("Hi my friend") << std::endl;
+    
+    /* PIG LATIN */
+    std::cout << PigLatin::Pig("banana") << std::endl;
+    
+    /* COUNT VOWELS */
+    std::cout << VowelCounter::VCounter("Magical software") << std::endl;
     return 0;
 }
 
